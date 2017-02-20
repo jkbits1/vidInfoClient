6f3303ec8ff0a7d5743f6d54b941a652e88aca39
jkbits1 <jkbits1@users.noreply.github.com>
2017-02-20 01:00:47 +0000

    basic app to get file details

---
+++ rx-app/NOTES.md
@@ -0,0 +1,6 @@
+
+misc html in temp.html
+
+
+edit in docker :e to sync with host editor, :w to write/alert watcher
+
--- rx-app/src/app/app.component.ts
+++ rx-app/src/app/app.component.ts
@@ -1,19 +1,18 @@
 import { Component } from '@angular/core';
 
-// import {FORM_DIRECTIVES, CORE_DIRECTIVES, Observable, EventEmitter} from 'angular2/angular2';
-// import {FORM_DIRECTIVES} from '@angular/core';
-import {Observable} from 'rxjs/Observable';
-// import { WheelCalcs } from './wheelCalcs';
+import { Subject, BehaviorSubject } from 'rxjs';
 
-// import { Subject, BehaviorSubject }    from 'rxjs5';
-import { Subject, BehaviorSubject }    from 'rxjs';
+// this import form is more efficient for bandwidth
+import {Observable} from 'rxjs/Observable';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app works, hooray xyz!';
+  title = 'Video Info Client';
 }
 
