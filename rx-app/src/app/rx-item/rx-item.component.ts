3da81ce0a8816a0bc353c4243b4ab2b238b54b0d
jkbits1 <jkbits1@users.noreply.github.com>
2017-02-20 01:59:03 +0000

    fileName type becomes String, tidying

--- rx-app/src/app/rx-item/rx-item.component.ts
+++ rx-app/src/app/rx-item/rx-item.component.ts
@@ -1,11 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
-// import {FORM_DIRECTIVES, CORE_DIRECTIVES, Observable, EventEmitter} from '@angular2/angular2';
 import {Observable, Subject, BehaviorSubject} from 'rxjs';
-// import {Http, URLSearchParams} from '@angular2/http';
-// import {Http, Jsonp} from '@angular/http';
 import {Http} from '@angular/http';
-// import {JSONP_PROVIDERS, Jsonp} from '@angular2/http';
 
 var url = "http://localhost:8000/vidInfoWrapped/";
 var urlCb = "?callback=JSON_CALLBACK";
@@ -15,127 +11,69 @@ var urlCb = "?callback=JSON_CALLBACK";
   templateUrl: './rx-item.component.html',
   styleUrls: ['./rx-item.component.css']
 })
+
 export class RxItemComponent implements OnInit {
-  // fileName: String = "";
-  fileName: any = "";
+  fileName: String = "";
   titleDetailsParsed: Array<any> = [];
   results: any = [];
-  // private searchesSubject = new BehaviorSubject<string>("");
+
   searchesSubject = new Subject<string>();
 
-  // constructor(http:Http, jsonp:Jsonp) { 
   constructor(http:Http) { 
-            this.searchesSubject.asObservable()
-            .debounceTime(500)
-            .distinctUntilChanged()
-            // pre-process fileName to prevent errors
-            .map(fileName => {
-                if (fileName === undefined || fileName.length === 0) {
-                    return "red-info.txt";
-                }
-                else {
-                    return fileName;
-                }
-            })
-            .switchMap(fileName => {
-                var url2 = url + fileName + urlCb;
+    this.searchesSubject.asObservable()
+    .debounceTime(500)
+    .distinctUntilChanged()
+    // pre-process fileName to prevent errors
+    .map(fileName => {
+      if (fileName === undefined || fileName.length === 0) {
+        return "red-info.txt";
+      }
+      else {
+        return fileName;
+      }
+    })
+    .switchMap(fileName => {
+      var url2 = url + fileName + urlCb;
 
-                return http.get(url2) //, {search: params}
-                    .map(res => {
-                        return res.json();
-                    });
-            })
-            // handle error checking in a specific Observable, 
-            // this is MUCH more elegant than the usual js way
-            .filter((data) => { 
-                if (data.fileName !== undefined && data.fileName.length !== 0)
-                    return true;
-                else
-                    return false;
-            })
-            .subscribe((data) => {
-                    console.log('Returned data: ' + data);
-                    this.fileName = data.fileName;
+      return http.get(url2) 
+        .map(res => {
+          return res.json();
+        });
+    })
+    // handle error checking in a specific Observable, 
+    // this is MUCH more elegant than the usual js way
+    .filter((data) => { 
+      if (data.fileName !== undefined && data.fileName.length !== 0)
+        return true;
+      else
+        return false;
+    })
+    .subscribe((data) => {
+        console.log('Returned data: ' + data);
+        this.fileName = data.fileName;
 
-                    this.titleDetailsParsed = JSON.parse(data.titleDetails);
-                    this.results =
-                        this.titleDetailsParsed
-                        .map((val, idx) => {
-                            return {
-                                titleNumber: val.titleNumber,
-                                length: val.length
-                            }
-                        });                                    
-                },
-                error => {
-                    console.error('Error connecting to server.');
-                },
-                () => {
-                    console.log('Completed!');
-            });
-
+        this.titleDetailsParsed = JSON.parse(data.titleDetails);
+        this.results =
+          this.titleDetailsParsed
+          .map((val, idx) => {
+            return {
+              titleNumber: val.titleNumber,
+              length: val.length
+            }
+          });                                    
+      },
+      error => {
+        console.error('Error connecting to server.');
+      },
+      () => {
+        console.log('Completed!');
+    });
   }
 
   ngOnInit() {
   }
 
-    keyup($event){
-        this.searchesSubject.next($event.currentTarget.value);
-    }
-
+  keyup($event){
+    this.searchesSubject.next($event.currentTarget.value);
+  }
 }
-
-// export class App {
-//     searches: EventEmitter = new EventEmitter();
-
-//     constructor(http:Http, jsonp:Jsonp) {
-//         this.searches._subject
-//             .debounceTime(500)
-//             .distinctUntilChanged()
-//             .switchMap(fileName => {
-//                 //working plunker http://plnkr.co/edit/P8dELQZ6HlglomXSvOcj?p=preview
-//                 var params = new URLSearchParams();
-                
-//                 var url2;
-//                 params.append('action', 'opensearch');
-//                 params.append('search', encodeURI(fileName));
-//                 params.append('format', 'json');
-//                 params.append('fileName', encodeURI(fileName));
-
-//                 //return jsonp.request(url, {search: params})
-//                 //return http.get("http://en.wikipedia.org/w/api.php?callback=JSON_CALLBACK", {search: params})
-
-//                 if (fileName === undefined || fileName.length === 0) {
-//                     fileName = "red-info.txt";
-//                 }
-                
-//                 url2 = url + fileName + urlCb;
-//                 return http.get(url2, {search: params})
-//                     .map(res => {
-//                         return res.json();
-//                     });
-//             })
-//             .subscribe((data) => {
-//                 console.log('Returned data: ' + data);
-//                 this.fileName = data.fileName;
-//                 this.titleDetailsParsed = JSON.parse(data.titleDetails);
-//                 this.results = 
-//                     // data.titleDetails
-//                     this.titleDetailsParsed
-//                     .map((val, idx) => {
-//                     return {
-//                         titleNumber: val.titleNumber,
-//                         length: val.length
-//                     }
-//                 });
-//             },
-//                 error => {
-//                     console.error('Error loading Wikipedia article.')
-//                 },
-//                 () => {
-//                     console.log('Completed!')
-//                 }
-//             );
-//     }
-
-// }
